13cd75734a0bda03b0d86e8752c84bfc4e7dcf26
thvanarkel <me@thomasvanarkel.nl>
2020-01-13 21:24:12 +0100

    Revert "Revert "Integrated RTC example""
    This reverts commit eab6cdbe1b08dd9fbc8d7f3d22a7ac17c9c19dbb.

--- Code/SleepLight/SleepLight.ino
+++ Code/SleepLight/SleepLight.ino
@@ -1,3 +1,15 @@
+#include <WiFiNINA.h>
+#include <RTCZero.h>
+
+RTCZero rtc;
+const int GMT = 1;
+
+#include "arduino_secrets.h">
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+
+int status = WL_IDLE_STATUS;
+
 enum State {
   LIGHT_IDLE,
   UNWINDING,
@@ -9,11 +21,45 @@ State currentState = LIGHT_IDLE;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.print("connecting");
+  while (status != WL_CONNECTED) {
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+    // wait 10 seconds for connection:
+    Serial.print(".");
+    delay(1000);
+  }
+  printWiFiStatus();
 
+  unsigned long epoch;
+  int numberOfTries = 0, maxTries = 6;
+  do {
+    epoch = WiFi.getTime();
+    Serial.println(epoch);
+    numberOfTries++;
+  }
+  while ((epoch == 0) && (numberOfTries < maxTries));
+
+  if (numberOfTries == maxTries) {
+    Serial.print("NTP unreachable!!");
+    while (1);
+  }
+  else {
+    Serial.print("Epoch received: ");
+    Serial.println(epoch);
+    rtc.setEpoch(epoch);
+
+    Serial.println();
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  printDate();
+  printTime();
+  Serial.println();
+  delay(1000);
   updateStateMachine();
 }
 
@@ -31,4 +77,49 @@ void updateStateMachine() {
     case AWAKENING:
       break;
   }
+}
+
+void printTime()
+{
+  print2digits(rtc.getHours() + GMT);
+  Serial.print(":");
+  print2digits(rtc.getMinutes());
+  Serial.print(":");
+  print2digits(rtc.getSeconds());
+  Serial.println();
+}
+
+void printDate()
+{
+  Serial.print(rtc.getDay());
+  Serial.print("/");
+  Serial.print(rtc.getMonth());
+  Serial.print("/");
+  Serial.print(rtc.getYear());
+
+  Serial.print(" ");
+}
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void print2digits(int number) {
+  if (number < 10) {
+    Serial.print("0");
+  }
+  Serial.print(number);
 }
