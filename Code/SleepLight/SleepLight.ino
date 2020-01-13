1b634837b676d2403bf70777003cbb0910c6f021
thvanarkel <me@thomasvanarkel.nl>
2020-01-13 12:45:51 +0100

    Basic state machine implementation

--- Code/SleepLight/SleepLight.ino
+++ Code/SleepLight/SleepLight.ino
@@ -1,3 +1,12 @@
+enum State {
+  LIGHT_IDLE,
+  UNWINDING,
+  SLEEPING,
+  AWAKENING
+};
+
+State currentState = LIGHT_IDLE;
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -5,5 +14,21 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  updateStateMachine();
+}
 
+void updateStateMachine() {
+  switch(currentState) {
+    case LIGHT_IDLE:
+      break;
+
+    case UNWINDING:
+      break;
+
+    case SLEEPING:
+      break;
+
+    case AWAKENING:
+      break;
+  }
 }
