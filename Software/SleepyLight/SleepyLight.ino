20f2d5a6c2a6e6726969cee9fe556edfea8641e0
thvanarkel <me@thomasvanarkel.nl>
2020-05-11 21:34:53 +0200

    Implement OTA sketch uploading and demo rainbow pattern

--- Software/SleepyLight/SleepyLight.ino
+++ Software/SleepyLight/SleepyLight.ino
@@ -1,11 +1,20 @@
-#include <ArduinoOTA.h>
 #include <WiFiNINA.h>
+#include <ArduinoOTA.h>
+#include <Adafruit_NeoPixel.h>
 
 #include "arduino_secrets.h">
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 
 int status = WL_IDLE_STATUS;
+
+#define LED_PIN    6
+#define LED_COUNT 50
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+unsigned long rainbowUpdate = 0;
+long hue = 0;
 
 enum State {
   LIGHT_IDLE,
@@ -20,6 +29,11 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.print("connecting");
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+
   while (status != WL_CONNECTED) {
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
@@ -27,19 +41,23 @@ void setup() {
     Serial.print(".");
     delay(1000);
   }
+
+  ArduinoOTA.begin(WiFi.localIP(), "Arduino", "password", InternalStorage);
+
   printWiFiStatus();
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println();
-  delay(1000);
+  ArduinoOTA.poll();
+
+  rainbow(10);
+
   updateStateMachine();
 }
 
 void updateStateMachine() {
-  switch(currentState) {
+  switch (currentState) {
     case LIGHT_IDLE:
       break;
 
@@ -69,4 +87,35 @@ void printWiFiStatus() {
   Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
+}
+
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  if (millis() < rainbowUpdate + wait) {
+    return;
+  }
+  rainbowUpdate = millis();
+
+  for (int i = 0; i < strip.numPixels(); i++) { // For each pixel in strip...
+    // Offset pixel hue by an amount to make one full revolution of the
+    // color wheel (range of 65536) along the length of the strip
+    // (strip.numPixels() steps):
+    int pixelHue = hue + (i * 65536L / strip.numPixels());
+    // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+    // optionally add saturation and value (brightness) (each 0 to 255).
+    // Here we're using just the single-argument hue variant. The result
+    // is passed through strip.gamma32() to provide 'truer' colors
+    // before assigning to each pixel:
+    strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+  }
+  strip.show(); // Update strip with new contents
+
+  if (hue < 5 * 65536) {
+    hue += 256;
+  } else {
+    hue = 0;
+  }
 }
