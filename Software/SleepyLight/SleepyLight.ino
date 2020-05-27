4e6a6177d5796d125926d22d99183b406e88709e
thvanarkel <me@thomasvanarkel.nl>
2020-05-27 10:02:53 +0200

    Implement alarm behaviour

--- Software/SleepyLight/SleepyLight.ino
+++ Software/SleepyLight/SleepyLight.ino
@@ -47,6 +47,7 @@ const char filename[] = "orbit.wav";
 SDWaveFile waveFile;
 
 Orientation orientation;
+boolean didTurn;
 
 enum Movement {
   PICKUP = 0,
@@ -54,15 +55,16 @@ enum Movement {
   TURN = 2
 };
 
-
 enum State {
   LIGHT_IDLE,
   UNWINDING,
   SLEEPING,
-  AWAKENING
+  AWAKENING,
+  ALARMED
 };
 
 State currentState = LIGHT_IDLE;
+State lastState;
 
 void setup() {
   // put your setup code here, to run once:
@@ -81,36 +83,35 @@ void setup() {
   }
   client.begin("broker.shiftr.io", net);
   client.onMessage(messageReceived);
-  connect();
+    connect();
 
   // Initialise RTC
-  //  Wire.begin();
-  //  if (!rtc.begin()) {
-  //    client.publish("/error", "Couldn't find RTC");
-  //  }
-  //  //
-  //  if (rtc.lostPower()) {
-  //    client.publish("/error", "RTC lost power, let's set the time!");
-  //    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  //  }
-  //  nextAlarm();
+  Wire.begin();
+  if (!rtc.begin()) {
+    client.publish("/error", "Couldn't find RTC");
+  }
+  //
+  if (rtc.lostPower()) {
+    client.publish("/error", "RTC lost power, let's set the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 
   // Initialise SD card reader and I2S speaker
-  //  if (!SD.begin()) {
-  //    client.publish("/error", "SD initialization failed");
-  //    //    return;
-  //  }
-  //  waveFile = SDWaveFile(filename);
-  //  if (!waveFile) {
-  //    client.publish("/error", "wave file invalid");
-  //  }
-  //  AudioOutI2S.volume(60);
-  //  if (!AudioOutI2S.canPlay(waveFile)) {
-  //    client.publish("/error", "unable to play wave file using I2S!");
-  //    //    while (1); // do nothing
-  //  }
-  //
-  //  //  // Initialise shutdown pin speaker
+  if (!SD.begin()) {
+    client.publish("/error", "SD initialization failed");
+    //    return;
+  }
+  waveFile = SDWaveFile(filename);
+  if (!waveFile) {
+    client.publish("/error", "wave file invalid");
+  }
+  AudioOutI2S.volume(60);
+  if (!AudioOutI2S.canPlay(waveFile)) {
+    client.publish("/error", "unable to play wave file using I2S!");
+    //    while (1); // do nothing
+  }
+
+  // Initialise shutdown pin speaker
   pinMode(SHUTDOWN_PIN, OUTPUT);
   digitalWrite(SHUTDOWN_PIN, LOW);
 
@@ -120,7 +121,12 @@ void setup() {
 
   ArduinoOTA.begin(WiFi.localIP(), "Arduino", "password", InternalStorage);
 
-  programUploaded();
+
+  updateOrientation();
+  detectTurn(orientation);
+
+  client.publish("/state", String(currentState));
+  lastState = currentState;
 }
 
 void connect() {
@@ -147,74 +153,96 @@ boolean disconnected = false;
 void loop() {
   ArduinoOTA.poll();
   lamp.tick();
+  Serial.println(lamp.level);
 
-  if (!client.connected()) {
-    connect();
-  }
+    if (!client.connected()) {
+      connect();
+    }
   client.loop();
 
-//  if (!AudioOutI2S.isPlaying()) {
-//    // playback has stopped
-//    digitalWrite(SHUTDOWN_PIN, LOW);
-//    // Doing this during I2S outputting sound willl drop connection
-////  }
-//  if (millis() - lastUpdate > 1000) {
-////    client.publish("/ledLevel", String(lamp.level));
-////    client.publish("/state", String(currentState));
-////    client.publish("/orientation", String(orientation));
-//    lastUpdate = millis();
-//  }
+  if (!AudioOutI2S.isPlaying()) {
+    // playback has stopped
+    digitalWrite(SHUTDOWN_PIN, LOW);
+    // Doing this during I2S outputting sound willl drop connection
+  }
 
-//  updateOrientation();
-//
-//
-//  detectMovement();
+  if (currentState != lastState) {
+    client.publish("/state", String(currentState));
+    lastState = currentState;
+  }
+
+  if (millis() - lastUpdate > 1000) {
+    client.publish("/ledLevel", String(lamp.level));
+    
+    //    client.publish("/orientation", String(orientation));
+    lastUpdate = millis();
+  }
+
+  updateOrientation();
+  //
+  //
+  detectMovement();
 
 
   //  //
   //  //  char buff[BUFF_MAX];
-  //  unsigned long n = millis();
-  //  if ((n - prev > interval)) {
-  //    DateTime now = rtc.now();
-  //    //
-  //    String time = String(now.day(), DEC) + "." + String(now.month(), DEC) + "." + String(now.year(), DEC) + ": " + String(now.hour(), DEC) + ":" + String(now.minute(), DEC) + ":" + String(now.second(), DEC);
-  //    //         // snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d", now.year, now.month, now.mday, now.hour, now.min, now.sec);
-  //    client.publish("/time", time);
-  //    if (rtc.alarmFired(1)) {
-  //      rtc.clearAlarm(1);
-  //      client.publish("/event/alarm", "true");
-  //      //
-  //      //      if (turnedOn) {
-  //      //        lamp.turnOff(8000);
-  //      //      } else {
-  //      //        lamp.turnOn(2000);
-  //      //      }
-  //      //      turnedOn = !turnedOn;
-  //      digitalWrite(SHUTDOWN_PIN, HIGH);
-  //      AudioOutI2S.play(waveFile);
-  //      //      //       nextAlarm();
-  //      //      //        // clear a2 alarm flag and let INT go into hi-z
-  //      //      //        // DS3231_clear_a2f();
-  //    }
-  //    prev = n;
-  //  }
+  unsigned long n = millis();
+  if ((n - prev > interval)) {
+    DateTime now = rtc.now();
+    //    //
+    String time = String(now.dayOfTheWeek(), DEC) + " " + String(now.day(), DEC) + "." + String(now.month(), DEC) + "." + String(now.year(), DEC) + ": " + String(now.hour(), DEC) + ":" + String(now.minute(), DEC) + ":" + String(now.second(), DEC);
+    //    //         // snprintf(buff, BUFF_MAX, "%d.%02d.%02d %02d:%02d:%02d", now.year, now.month, now.mday, now.hour, now.min, now.sec);
+    client.publish("/time", time);
+    //      if (rtc.alarmFired(1)) {
+    //        rtc.clearAlarm(1);
+    //        client.publish("/event/alarm", "true");
+    //  //      //
+    //  //      //      if (turnedOn) {
+    //  //      //        lamp.turnOff(8000);
+    //  //      //      } else {
+    //  //      //        lamp.turnOn(2000);
+    //  //      //      }
+    //  //      //      turnedOn = !turnedOn;
+    //        digitalWrite(SHUTDOWN_PIN, HIGH);
+    //        AudioOutI2S.play(waveFile);
+    //  //      //      //       nextAlarm();
+    //  //      //      //        // clear a2 alarm flag and let INT go into hi-z
+    //  //      //      //        // DS3231_clear_a2f();
+    //      }
+    prev = n;
+  }
 
   updateStateMachine();
 
-  //  delay(20);
+  delay(10);
 }
 
 void updateStateMachine() {
   switch (currentState) {
     case LIGHT_IDLE:
-//      if (detectTurn(orientation)) {
-//        lamp.orientation = orientation;
-//        lamp.turnOn(2000);
-//      }
-//      if (lamp.level >= 1023) {
-//        currentState = UNWINDING;
-//        lamp.turnOff(10000);
-//      }
+      if (detectTurn(orientation)) {
+        lamp.orientation = orientation;
+        lamp.turnOn(2000);
+        didTurn = true;
+      }
+      if (didTurn) {
+        if (lamp.level >= 1023) {
+          didTurn = false;
+          currentState = UNWINDING;
+          lamp.turnOff(10000);
+        }
+      }
+
+
+      if (rtc.alarmFired(1)) {
+        rtc.clearAlarm(1);
+        DateTime now = rtc.now();
+        DateTime future(now + TimeSpan(0, 0, 30, 0)); // TODO: fix the times
+        rtc.setAlarm1(future, DS3231_A1_Hour);
+        lamp.turnOn(30 * 60 * 1000); // TODO: fix the times
+        currentState = AWAKENING;
+      }
+
       break;
 
     case UNWINDING:
@@ -227,13 +255,31 @@ void updateStateMachine() {
       break;
 
     case AWAKENING:
+      if (rtc.alarmFired(1)) {
+        rtc.clearAlarm(1);
+        digitalWrite(SHUTDOWN_PIN, HIGH);
+        AudioOutI2S.play(waveFile);
+        // TODO: set the alarm for the next day!
+        // TODO: check for calendar days
+        currentState = ALARMED;
+      }
+      break;
+
+    case ALARMED:
+      if (!AudioOutI2S.isPlaying()) {
+        // playback has stopped
+        digitalWrite(SHUTDOWN_PIN, HIGH);
+        AudioOutI2S.play(waveFile);
+      }
+      if (detectTurn(orientation)) {
+        AudioOutI2S.stop();
+        digitalWrite(SHUTDOWN_PIN, LOW);
+        lamp.orientation = orientation;
+        lamp.turnOff(2000);
+        currentState = LIGHT_IDLE;
+      }
       break;
   }
-}
-
-void programUploaded() {
-  //  digitalWrite(SHUTDOWN_PIN, HIGH);
-  //  AudioOutI2S.play(waveFile);
 }
 
 void nextAlarm()
@@ -263,8 +309,14 @@ String getValue(String data, char separator, int index)
 
 void setAlarm(String str) {
   //(year, month, day, hour, minute, second)
-  DateTime a(0, 0, 0, getValue(str, ':', 0).toInt(), getValue(str, ':', 1).toInt(), 0);
-  rtc.setAlarm1(a, DS3231_A1_Hour);
+  rtc.clearAlarm(1);
+  DateTime now = rtc.now();
+  DateTime a(now.year(), now.month(), now.day(), getValue(str, ':', 0).toInt(), getValue(str, ':', 1).toInt(), 0);
+  DateTime future = a - TimeSpan(0, 0, 30, 0);
+  rtc.setAlarm1(future, DS3231_A1_Hour);
+  char buffer[] = "hh:mm:ss";
+  String msg = future.toString(buffer);
+  client.publish("/light-alarm", msg);
 }
 
 void messageReceived(String &topic, String &payload) {
@@ -274,7 +326,7 @@ void messageReceived(String &topic, String &payload) {
     if (payload.equals("true") && lamp.level <= 0) {
       lamp.turnOn(2000);
     } else if (payload.equals("false") && lamp.level >= 1023) {
-      lamp.turnOff(5 * 60 * 1000);
+      lamp.turnOff(2000);
     }
   } else if (topic.equals("/alarm")) {
     setAlarm(payload);
